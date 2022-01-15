83f86963c6012a6946a60952b117879520c45470
RelishedChicken <veerajvadgama@gmail.com>
2022-01-15 19:07:35 +0000

    implemented button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './media/font.otf'
 import './media/banner.png';
 import logo from './media/logo.svg'
 import introVideo from './media/introVideo.mp4';
+import button from './media/button.svg'
 import React from 'react';
+import config from './config.json';
 import {useState, useEffect} from 'react';
 import $ from 'jquery';
 
@@ -15,13 +17,13 @@ function App() {
 
   const [introArray, setIntroArray] = useState(
     [
-      "Hi.",
+      /*"Hi.",
       "I'm sure you're here for the same reason as many.",
       "Sick of seeing lions and apes that look like they were drawn by three year olds?",
       "Annoyed of seeing CryptoBros circle-jerking thier way to the 'top'?",
       "Just want a much better Twitter timeline?",
       "Whatever the reason, NFT Nuke has you covered.",      
-      "Coming soon to an ad-ridden browser near you."
+      "So go ahead. Atomize those CryptoBros."*/
     ]
   );
 
@@ -42,7 +44,8 @@ function App() {
   //Start video
   const start = () => {
     $('.enter').fadeOut(()=>{
-      $('.enterWrapper').remove();
+      $('.enterWrapper').remove();      
+      $('.apeCape').css('opacity', '0.8');
       $('#videoWrapper').fadeIn();
       document.getElementById('video').play();
     });
@@ -59,27 +62,67 @@ function App() {
 
   //Render intro
   const intro = () => {    
-    $('#videoWrapper').fadeOut();    
+    //Hide the video
+    $('#videoWrapper').fadeOut();
     $('#videoWrapper').remove();
+
+    //Fade in paragraphs, menu and footer
     $('#introWrapper').fadeIn();
     $('.apeCape').css('opacity', '0.5');
     $('.menu').fadeIn();    
     $('.footer').fadeIn();
-    console.log("started intro....");
-    //for each into sentence
+    
+    //Grab and show a random slogan
     let randomSlogan = slogans[Math.floor(Math.random() * slogans.length)];
     $('#slogan').text(randomSlogan);
 
+    //Loop through 'story' and display
     introArray.forEach((introSentenceAvailable, i) => {
+        //Time calculation to show each sentence
         setTimeout(()=>{
           $('#fadingSentence').fadeOut(()=>{
             $('#fadingSentence').text(introSentenceAvailable)
             $('#fadingSentence').fadeIn();            
           });
-        }, i * 5000);
-    });
+        }, i * 4000);
+    })
+    
+    setTimeout(() => {
+      enableButton();
+    }, introArray.length * 4000)
+  
+
   }
 
+  const enableButton = () => {
+    //Remove story elements    
+    $('#introWrapper').fadeOut();
+    $('#introWrapper').remove();
+
+    //Show button
+    $('#buttonWrapper').fadeIn();
+    
+
+    console.log("Button now available..");
+
+  }
+
+  const nuke = () => {
+    console.log("boom");
+
+    //Nuke spin loop
+    let deg = 0;
+
+    
+    setInterval(() => {      
+      deg += 1;
+      $('.button').css('transform', 'rotate('+deg+'deg)');      
+      if(deg === 360){
+        deg = 0;
+      }
+    }, 2);
+
+  }
 
   //Render
   return (
@@ -90,7 +133,7 @@ function App() {
               <img className='logo' src={logo} alt='NFT Nuke Logo'/>
             </div>
             <div className='menuItem'>
-              <h1 className='pageTitle'>NFT Blocker</h1> 
+              <h1 className='pageTitle'>NFT Nuke</h1> 
             </div>            
             <div className='menuItemSeperator'></div>            
             <div className='menuItem'>
@@ -107,6 +150,9 @@ function App() {
             </div>
             <div hidden id='introWrapper' className='intro'>
               <p id="fadingSentence"></p>
+            </div>
+            <div hidden id='buttonWrapper'>              
+              <img onClick={nuke} className='button' src={button} alt='Press to Nuke'></img>
             </div>
             <div hidden className='footer'>
               <p>No apes were harmed in the making of this | <a target='_blank' className='link' href='https://twitter.com/_ThomasPearson_'>Thomas Pearson</a> & <a target='_blank' className='link' href='https://twitter.com/vadgamaveeraj'>V</a></p>
